58375717665457eae2606d00cada18be9306f041
PODISHETTI SAI SANTHOSH <87226601+podishettisaisanthosh@users.noreply.github.com>
2023-08-29 12:03:00 +0530

    Add files via upload

---
+++ referlancer/connect.php
@@ -0,0 +1,44 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "localhost";
+$dbusername = "epiz_29108052";
+$dbpassword = "Pranitesa@#1432";
+$dbname = "epiz_29108052_mantri	";
+// Create connectioN
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO account (username, password)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo '<script>alert("Low Balance\nRecharge account and try again")</script>';
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
+<html>
+<head>
+
+</html>
